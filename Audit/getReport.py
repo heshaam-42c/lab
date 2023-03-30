8f7c9cb7466cac0fb598a3f847da078bc1bb772c
Heshaam Attar <heshaam.attar@42crunch.com>
2023-03-30 09:28:03 -0700

    Add uuid

--- Audit/getReport.py
+++ Audit/getReport.py
@@ -92,7 +92,7 @@ fileString += '\nTimestamp: '+str(datetime.fromtimestamp(int(jsonResponse['date'
 
 fileString += '\n\n[<kbd> <br> Report <br> </kbd>][Link]'
 
-fileString += '\n\n[Link]: https://demolabs.42crunch.cloud/apis/9273b0fc-ab90-4325-9fcb-a6f504e07840/security-audit-report'
+fileString += '\n\n[Link]: https://demolabs.42crunch.cloud/apis/'+uuid+'/security-audit-report'
 
 # Build summary report table
 for sqgReport in jsonResponse['processingDetails']:
